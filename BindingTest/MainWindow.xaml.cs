d0502935ca2a3ce0d8e614dd6bea5866d93b1745
zxx <357133157@qq.com>
2022-01-18 16:25:59 +0800

    12

--- BindingTest/MainWindow.xaml.cs
+++ BindingTest/MainWindow.xaml.cs
@@ -8,28 +8,28 @@ namespace BindingTest
     /// </summary>
     public partial class MainWindow : Window
     {
-        public myClass mcl { get; set; } = new myClass();
+        public MyClass Mcl { get; set; } = new MyClass();
         public MainWindow()
         {
             InitializeComponent();
-            mcl.mytext = "hello";
-            mygrid.DataContext = mcl;//需要给mygrid控件指定DataContext为mcl，这是告诉Grid控件以及它子控件绑定源是谁
+            Mcl.Mytext = "hello";
+            mygrid.DataContext = Mcl;//需要给mygrid控件指定DataContext为mcl，这是告诉Grid控件以及它子控件绑定源是谁
         }
     }
 
-    public class myClass : INotifyPropertyChanged
+    public class MyClass : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         private string _mytext;
-        public string mytext
+        public string Mytext
         {
             get
             { return _mytext; }
             set
             {
                 _mytext = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("mytext"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Mytext"));
             }
         }
     }
